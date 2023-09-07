3cb38ad73a9b212b26187e3edc581acbdc3b3a4e
Ozan Karaali <ozan.karaali@gmail.com>
2023-09-07 23:28:43 +0200

    added hold gestures, put double click to middle finger sign just in case if fast clicks do not work, also it's more smooth now.

--- main.py
+++ main.py
@@ -4,80 +4,83 @@ import mediapipe as mp
 import pyautogui
 import time
 
+# lag protection
+pyautogui.PAUSE = 0
+pyautogui.MINIMUM_SLEEP = 0
 
 # Constants
 screen_width, screen_height = pyautogui.size()
 margin = 150
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7)  # Increase thresholds for performance
+hands = mp_hands.Hands()
 mp_drawing = mp.solutions.drawing_utils
 
-click_threshold = 0.5
 cooldown_period = 0.7
-
-last_okay_time, last_yoga_time, last_click_time = 0, 0, 0
-gesture_states = {'okay_sign': False, 'yoga_sign': False}
+last_release_time, last_click_time = 0, 0
 
 
 def get_gesture_landmark_distances(landmarks):
     """Calculate distances between thumb and other fingertips for gesture detection."""
-    distance_okay = abs(
+    distance_index = abs(
         landmarks[mp_hands.HandLandmark.INDEX_FINGER_TIP].x - landmarks[mp_hands.HandLandmark.THUMB_TIP].x) + abs(
         landmarks[mp_hands.HandLandmark.INDEX_FINGER_TIP].y - landmarks[mp_hands.HandLandmark.THUMB_TIP].y)
-    distance_yoga = abs(
+    distance_middle = abs(
         landmarks[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].x - landmarks[mp_hands.HandLandmark.THUMB_TIP].x) + abs(
         landmarks[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y - landmarks[mp_hands.HandLandmark.THUMB_TIP].y)
-    return distance_okay, distance_yoga
+    distance_ring = abs(
+        landmarks[mp_hands.HandLandmark.RING_FINGER_TIP].x - landmarks[mp_hands.HandLandmark.THUMB_TIP].x) + abs(
+        landmarks[mp_hands.HandLandmark.RING_FINGER_TIP].y - landmarks[mp_hands.HandLandmark.THUMB_TIP].y)
+    return distance_index, distance_middle, distance_ring
 
 
 def process_gesture(landmarks):
     """Detect and process gesture."""
-    distance_okay, distance_yoga = get_gesture_landmark_distances(landmarks)
-    threshold_okay, threshold_yoga = 0.05, 0.05
+    global last_release_time
+    distance_index, distance_middle, distance_ring = get_gesture_landmark_distances(landmarks)
+    threshold_index, threshold_middle, threshold_ring = 0.06, 0.06, 0.06
     current_time = time.time()
 
     # Okay Sign
-    if distance_okay < threshold_okay:
-        return handle_okay_sign_gesture(current_time)
+    if distance_index < threshold_index:
+        return handle_index_gesture(current_time)
 
     # Yoga Sign
-    if distance_yoga < threshold_yoga:
-        return handle_yoga_sign_gesture(current_time)
+    if distance_middle < threshold_middle:
+        return handle_middle_gesture(current_time)
 
+    if distance_ring < threshold_ring:
+        return handle_ring_gesture(current_time)
+
+    last_release_time = current_time
     return None
 
 
-def handle_okay_sign_gesture(current_time):
+def handle_index_gesture(current_time):
     """Handle logic specific to okay sign gesture."""
-    global last_okay_time, last_click_time, gesture_states
-    if current_time - last_okay_time < click_threshold:
-        last_okay_time = 0
+    global last_release_time
+    pyautogui.click()
+    if current_time - last_release_time > cooldown_period:
+        return 'Left Click Hold'
+    return 'Left Click'
+
+
+def handle_middle_gesture(current_time):
+    """Handle logic specific to yoga sign gesture."""
+    global last_release_time
+    pyautogui.click(button='right')
+    if current_time - last_release_time > cooldown_period:
+        return 'Right Click Hold'
+    return 'Right Click'
+
+
+def handle_ring_gesture(current_time):
+    """Handle logic specific to yoga sign gesture."""
+    global last_click_time
+    if current_time - last_click_time > cooldown_period:
         last_click_time = current_time
         pyautogui.doubleClick()
-        return 'Double Left Click'
-    elif current_time - last_click_time > cooldown_period:
-        last_okay_time = current_time
-        last_click_time = current_time
-        pyautogui.click()
-        return 'Left Click'
-    return None
-
-
-def handle_yoga_sign_gesture(current_time):
-    """Handle logic specific to yoga sign gesture."""
-    global last_yoga_time, last_click_time, gesture_states
-    if current_time - last_yoga_time < click_threshold:
-        last_yoga_time = 0
-        last_click_time = current_time
-        pyautogui.doubleClick(button='right')
-        return 'Double Right Click'
-    elif current_time - last_click_time > cooldown_period:
-        last_yoga_time = current_time
-        last_click_time = current_time
-        pyautogui.click(button='right')
-        return 'Right Click'
-    return None
+    return 'Double Click'
 
 
 def map_to_screen(x, y, frame_width, frame_height):
@@ -87,8 +90,12 @@ def map_to_screen(x, y, frame_width, frame_height):
     return int(mapped_x), int(mapped_y)
 
 
-def handle_multiple_hands(landmarks_list, handedness_list):
+def handle_multiple_hands(landmarks_list, handedness_list, left=False):
     """Returns the right hand's landmarks if present, otherwise the first hand's landmarks."""
+    if left:
+        for index, handedness in enumerate(handedness_list):
+            if handedness.classification[0].label == 'Left':
+                return landmarks_list[index]
     for index, handedness in enumerate(handedness_list):
         if handedness.classification[0].label == 'Right':
             return landmarks_list[index]
@@ -98,14 +105,17 @@ def handle_multiple_hands(landmarks_list, handedness_list):
 # Add argparse for command-line toggling
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Hand Gesture Mouse Control")
-    parser.add_argument('--display', action='store_true', help="Display the webcam feed with hand landmarks.")
+    parser.add_argument('--nodisplay', action='store_true', help="Display the webcam feed with hand landmarks.")
+    parser.add_argument('--lefthanded', action='store_true', help="Only track the left hand.")
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-
+    frame_width, frame_height = 640, 480
     cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -118,13 +128,19 @@ def main():
 
         # Process hands
         if results.multi_hand_landmarks and results.multi_handedness:
-            landmarks = handle_multiple_hands(results.multi_hand_landmarks, results.multi_handedness)
+            landmarks = handle_multiple_hands(results.multi_hand_landmarks, results.multi_handedness, args.lefthanded)
             gesture = process_gesture(landmarks.landmark)
             index_mcp_x = int(landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].x * frame_width)
             index_mcp_y = int(landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y * frame_height)
             mapped_x, mapped_y = map_to_screen(index_mcp_x, index_mcp_y, frame_width, frame_height)
-            pyautogui.moveTo(mapped_x, mapped_y)
-            # pyautogui.moveTo(mapped_x, mapped_y, duration=0.1)  # Add duration for smoother movement
+            if gesture == 'Left Click Hold':
+                pyautogui.dragTo(mapped_x, mapped_y, button='left')
+            elif gesture == 'Right Click Hold':
+                pyautogui.dragTo(mapped_x, mapped_y, button='right')
+            # elif gesture == 'Double Click':
+            #   continue
+            else:
+                pyautogui.moveTo(mapped_x, mapped_y)
             mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
             if gesture:
                 cv2.putText(frame, gesture, (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
@@ -133,12 +149,7 @@ def main():
             cv2.putText(frame, f"y:{index_mcp_y}", (frame_width - 100, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0),
                         1)
 
-        # If display is turned on via command line, bypassing it to debug release
-        if True:
-            # Resize the frame for faster display
-            display_scale = 0.5  # Adjust this value as needed
-            frame = cv2.resize(frame, (int(frame_width * display_scale), int(frame_height * display_scale)))
-
+        if not args.nodisplay:
             cv2.imshow('Handroller', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
